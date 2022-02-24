66aee1b4e9debf2344d01c70d0e378defad4e3f6
Andrei Piskunov <explorer67@mail.ru>
2022-02-24 15:30:15 +0300

    Initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        final int min = 90; // Минимальное число для диапазона
+        final int max = 100; // Максимальное число для диапазона
+        for (int r : new Randoms(min, max)) {
+            System.out.println("Случайное число: " + r);
+            if (r == max) {
+                System.out.printf("Выпало число %d, давайте на этом закончим\n", max);
+                break;
+            }
+        }
+    }
+}
